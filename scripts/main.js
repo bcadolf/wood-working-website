af6d6d3a806c53585aa0bd94e37fd3efec1d23f6
bcadolf <adolfbrandon96@gmail.com>
2024-10-29 15:28:30 -0400

    project img animations

--- scripts/main.js
+++ scripts/main.js
@@ -39,4 +39,34 @@ projects.addEventListener("click", () => {
     localStorage.setItem("title", projects.textContent)
 });
 
+const projImgs = document.querySelectorAll('.proj-img');
+let currentIndex = 0;
+
+if (projImgs) {
+    const updateImages = () => {
+        projImgs.forEach((img, i) => {
+            img.classList.remove('partial');
+            const offset = (i - currentIndex) % projImgs.length;
+            img.style.transform = `translateX(${offset * 100}%)`;
+        });
+        projImgs[(currentIndex + 1) % projImgs.length].classList.add('partial');
+        projImgs[(currentIndex + projImgs.length - 1) % projImgs.length].classList.add('partial');
+    };
+
+    projImgs.forEach((img, index) => {
+        img.style.transform = `translateX(${100 * index}%)`;
+
+        img.addEventListener('click', () => {
+            if (img.classList.contains('partial')) {
+                currentIndex = index;
+                updateImages();
+            }
+        });
+    });
+
+    updateImages(); // Initial setup
+}
+
+
+
 
